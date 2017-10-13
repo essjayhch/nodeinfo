a60516170a73c4483d5a33b1adbee6cb3f787b3b
Stuart Harland <s.harland@livelinktechnology.net>
2017-10-13 11:36:43 +0100

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name             'nodeinfo'
+maintainer       'Stuart Harland'
+maintainer_email 'essjayhch@gmail.com'
+license          'All rights reserved'
+description      'Manages information about this node'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/essjayhch/nodeinfo'
+issues_url       'https://github.com/essjayhch/nodeinfo/issues'
+supports         'debian'
+supports         'rhel'
+supports         'centos'
+supports         'fedora'
+chef_version     '>= 12'
+version          '0.1.0'
+
+depends 'ohai'
